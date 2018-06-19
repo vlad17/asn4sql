53563c18d8d43f7e771ca46a44a8ecec94b8612d
Vladimir Feinberg <vladimir.feinberg@gmail.com>
2018-06-18 22:19:33 -0700

    added checkpointing; testing

--- asn4sql/main/wikisql_specific.py
+++ asn4sql/main/wikisql_specific.py
@@ -6,15 +6,14 @@ Assumes that the processed-toy(0|1).pth dataset exists in
 {dataroot}/wikisql/ already.
 """
 
+from contextlib import closing
 import os
-import sys
+import shutil
 import itertools
 
 from absl import app
 from absl import flags
-from contextlib import closing
 import torch
-from torch import optim
 import torch.utils
 from tqdm import tqdm
 import numpy as np
@@ -31,13 +30,13 @@ flags.DEFINE_integer('seed', 1, 'random seed')
 flags.DEFINE_boolean('toy', False, 'use a toy dataset for debugging')
 
 # training logistics
-# flags.DEFINE_string('restore_checkpoint', None, 'checkpoint to restore '
-#                     'training from')
-# flags.DEFINE_integer(
-#     'persist_every', 10,
-#     'period of mini-batches between checkpoint persists (0 to disable)')
-flags.DEFINE_integer('evaluate_every', 5,
-                     'period of epochs between evaluations (0 to disable)')
+flags.DEFINE_string(
+    'restore_checkpoint', None,
+    'restore training from this checkpoint; the current best '
+    'from the restored run must be in the same directory ')
+flags.DEFINE_integer(
+    'persist_every', 10,
+    'period of epochs between checkpoint persists (0 to disable)')
 flags.DEFINE_integer('max_epochs', 100,
                      'maximum number of epochs for training')
 flags.DEFINE_integer(
@@ -76,6 +75,10 @@ def _main(argv):
         model.to(torch.device('cpu')),
         os.path.join(log.logging_directory(), 'untrained_model.pth'))
     model = model.to(device)
+    training_state = _TrainingState()
+    if flags.FLAGS.restore_checkpoint:
+        _copy_best_checkpoint(flags.FLAGS.restore_checkpoint)
+        _load_checkpoint(flags.FLAGS.restore_checkpoint, model, training_state)
     model = model.share_memory()
 
     num_workers = flags.FLAGS.workers
@@ -83,18 +86,12 @@ def _main(argv):
     with closing(SyncTrainer(model, num_workers)) as trainer:
         trainer.zero_grad()
         log.debug('all {} remote workers initialized', num_workers)
-        _do_training(model, train, val, trainer)
+        _do_training(model, train, val, trainer, training_state)
 
 
-def _do_training(model, train, val, trainer):
-
-    # training_state = _TrainingState()
-    # if flags.FLAGS.restore_checkpoint:
-    #     log.debug('restoring model from {}', flags.FLAGS.restore_checkpoint)
-    #     _load_checkpoint(flags.FLAGS.restore_checkpoint, model, optimizer,
-    #                      training_state)
-
+def _do_training(model, train, val, trainer, training_state):
     batch_size = flags.FLAGS.batch_size
+
     loss_window = RollingAverageWindow(len(train) // 10 // batch_size)
     acc_window = RollingAverageWindow(len(train) // 10 // batch_size)
     grad_window = RollingAverageWindow(len(train) // 10 // batch_size)
@@ -107,14 +104,16 @@ def _do_training(model, train, val, trainer):
         }
 
     model.train()
-
-    trainer.lr(flags.FLAGS.learning_rate)
+    trainer.lr(training_state.lr)
     perm = np.arange(len(train))
-    for epoch in range(1, 1 + flags.FLAGS.max_epochs):
+
+    for epoch in range(1 + training_state.epoch, 1 + flags.FLAGS.max_epochs):
         epochfmt = intfmt(flags.FLAGS.max_epochs)
+        training_state.epoch = epoch
         log.debug('begin epoch ' + epochfmt, epoch)
         # one sample at a time greatly simplifies pytorch seq2seq!
         np.random.shuffle(perm)
+
         samples = (train[i] for i in perm)
         with tqdm(total=len(train), postfix=_tqdm_postfix()) as progbar:
             for exs in _chunkify(samples, batch_size):
@@ -126,25 +125,32 @@ def _do_training(model, train, val, trainer):
                 progbar.update(len(exs))
                 progbar.set_postfix(**_tqdm_postfix())
 
-        if _check_period(epoch, flags.FLAGS.evaluate_every):
-            model.eval()
-            val_diagnostics = _diagnose(val, model)
-            train_diagnositcs = _diagnose(train, model, len(val))
-            val_diagnostics = _str_diagnostics('val', val_diagnostics)
-            train_diagnositcs = _str_diagnostics('(sampled) train',
+        model.eval()
+        val_diagnostics = _diagnose(val, model)
+        train_diagnositcs = _diagnose(train, model, len(val))
+        val_diagnostics_str = _str_diagnostics('val', val_diagnostics)
+        train_diagnositcs_str = _str_diagnostics('(sampled) train',
                                                  train_diagnositcs)
-            log.debug('epoch ' + epochfmt + ' of ' + epochfmt + '\n{}\n{}',
-                      epoch, flags.FLAGS.max_epochs, val_diagnostics,
-                      train_diagnositcs)
-            model.train()
+        log.debug('epoch ' + epochfmt + ' of ' + epochfmt + '\n{}\n{}', epoch,
+                  flags.FLAGS.max_epochs, val_diagnostics_str,
+                  train_diagnositcs_str)
+        model.train()
 
-        # if _check_period(training_state.batch_idx, flags.FLAGS.persist_every):
-        #     fmt = '{:0' + str(len(str(flags.FLAGS.max_batches))) + 'd}.pth'
-        #     checkpoint_file = os.path.join(
-        #         log.logging_directory(), 'checkpoints',
-        #         fmt.format(training_state.batch_idx))
-        #     log.debug('persisting model to {}', checkpoint_file)
-        #     _save_checkpoint(checkpoint_file, model, optimizer, training_state)
+        cur_val_loss = val_diagnostics['loss (*total)'][0]
+        if cur_val_loss < training_state.best_val_loss:
+            training_state.best_val_loss = cur_val_loss
+            best_file = _checkpoint_file('best.pth')
+            log.debug('updating best model into file {}', best_file)
+            _save_checkpoint(best_file, model, training_state)
+        else:
+            # TODO: early stopping with patience
+            # TODO: decay learning rate by 1/10
+            pass
+
+        if _check_period(epoch, flags.FLAGS.persist_every):
+            checkpoint_file = _checkpoint_file(epochfmt.format(epoch))
+            log.debug('persisting model to {}', checkpoint_file)
+            _save_checkpoint(checkpoint_file, model, training_state)
 
 
 def _diagnose(dataset, model, subsample=None):
@@ -203,34 +209,57 @@ def _check_period(idx, period):
     return idx == 1 or idx == flags.FLAGS.max_epochs or idx % period == 0
 
 
-# def _load_checkpoint(checkpoint_file, model, optimizer, training_state):
-#     state_dict = torch.load(checkpoint_file)
-#     model.load_state_dict(state_dict['model'])
-#     optimizer.load_state_dict(state_dict['optimizer'])
-#     training_state.load_state_dict(state_dict['training_state'])
+def _checkpoint_file(basename):
+    checkpoint_file = os.path.join(log.logging_directory(), 'checkpoints',
+                                   basename)
+    return checkpoint_file
 
-# def _save_checkpoint(checkpoint_file, model, optimizer, training_state):
-#     state_dict = {
-#         'model': model.state_dict(),
-#         'optimizer': optimizer.state_dict(),
-#         'training_state': training_state.state_dict()
-#     }
 
-#     os.makedirs(os.path.dirname(checkpoint_file), exist_ok=True)
-#     with open(checkpoint_file, 'wb') as f:
-#         torch.save(state_dict, f)
+def _copy_best_checkpoint(checkpoint_file):
+    bestfile = os.path.join(os.path.dirname(checkpoint_file), 'best.pth')
+    if not os.path.isfile(bestfile):
+        raise ValueError(
+            'was expecting checkpoint file {} to have a sibling '
+            'file best.pth for the running best model'.format(checkpoint_file))
+    best_dst = _checkpoint_file('best.pth')
+    log.debug('copying best running model file from {} to {}', bestfile,
+              best_dst)
+    os.makedirs(os.path.dirname(checkpoint_file), exist_ok=True)
+    shutil.copyfile(bestfile, best_dst)
 
-# class _TrainingState:
-#     def __init__(self):
-#         self.batch_idx = 0
 
-#     def state_dict(self):
-#         """return all training state for algorithm"""
-#         return {'batch_idx': self.batch_idx}
+def _load_checkpoint(checkpoint_file, model, training_state):
+    log.debug('restoring model from {}', flags.FLAGS.restore_checkpoint)
+    state_dict = torch.load(checkpoint_file)
+    model.load_state_dict(state_dict['model'])
+    training_state.load_state_dict(state_dict['training_state'])
 
-#     def load_state_dict(self, d):
-#         """re-load training state from dictionary"""
-#         self.batch_idx = d['batch_idx']
+
+def _save_checkpoint(checkpoint_file, model, training_state):
+    state_dict = {
+        'model': model.state_dict(),
+        'training_state': training_state.state_dict()
+    }
+
+    os.makedirs(os.path.dirname(checkpoint_file), exist_ok=True)
+    with open(checkpoint_file, 'wb') as f:
+        torch.save(state_dict, f)
+
+
+class _TrainingState:
+    def __init__(self):
+        self.epoch = 0
+        self.lr = flags.FLAGS.learning_rate
+        self.best_val_loss = np.inf
+
+    def state_dict(self):
+        """return all training state for algorithm"""
+        return self.__dict__
+
+    def load_state_dict(self, d):
+        """re-load training state from dictionary"""
+        self.__dict__.update(d)
+
 
 if __name__ == '__main__':
     app.run(_main)
