59ab164239a741a97e139a4d7722c05e204a6ed1
Vladimir Feinberg <vlad17@users.noreply.github.com>
2018-07-03 21:03:10 -0700

    rearchitect the decoder (#17)
    * independent attention for inputs
    
    * separate decoder arch
    
    * format
    
    * numpy interop bugfix
    
    * torch.tensor -> torch.Tensor (style)
    
    * truncate correctly
    
    * argmax axis
    
    * more pytorch empty tensor handling

--- asn4sql/wikisql_specific/condition_decoder.py
+++ asn4sql/wikisql_specific/condition_decoder.py
@@ -8,185 +8,234 @@ import functools
 from absl import flags
 import torch
 from torch import nn
-from torch.nn import functional as F
 
 from .mlp import MLP
 from .pointer import Pointer
-from .double_attention import double_attention
+from .double_attention import double_attention, IndependentAttention
 from ..data import wikisql
 from ..utils import get_device
 
-flags.DEFINE_integer('decoder_size', 128, 'hidden state size for the decoder')
+flags.DEFINE_integer('decoder_size', 64, 'hidden state size for the decoder')
 flags.DEFINE_integer('op_embedding_size', 16,
                      'embedding size for conditional operators')
 
+# pytorch convenience helpers
 
-class ConditionDecoder(nn.Module):
-    """nt_
-    The decoder produces seq2seq-style output for the conditional
-    columns in WikiSQL; it has quite a few moving parts:
 
-                            new output
-                                ^
-                                |
-    (decoder hidden state) -> LSTM -> (new hidden state)
-                                ^
-                                |
-    previous output   -> prepared input
+def _double_attn(*args):
+    return _Sequential(_Unwrap(double_attention(*args)), _Cat())
 
-    Where the input and output encapsulate the index of the
-    conditional operator for the conditional statement,
-    the index of the column within the current table,
-    the span across the question for the literal of the
-    corresponding conditional statement, and whether
-    to stop the decoding (all the conditional columns
-    have been generated).
-    """
 
-    def __init__(self, col_seq_size, src_seq_size):
+class _Unwrap(nn.Module):
+    # nn.Sequential only allows one argument for submodules, this lets us
+    # pass multiple args with tuples
+    def __init__(self, module):
         super().__init__()
-        num_words = len(wikisql.CONDITIONAL)
-        self.op_embedding = nn.Embedding(num_words,
-                                         flags.FLAGS.op_embedding_size)
-        self.src_col_attn = double_attention(src_seq_size, col_seq_size,
-                                             flags.FLAGS.decoder_size)
+        self.module = module
 
-        input_size = flags.FLAGS.decoder_size
-        self.stop_logits = MLP(input_size, [], 2)
-        self.op_logits = MLP(input_size, [], len(wikisql.CONDITIONAL))
-        input_size += self.op_logits.output_size
-        self.col_ptr_logits = Pointer(col_seq_size, input_size)
-        input_size += col_seq_size  # add an attention context
-        self.span_l_ptr_logits = Pointer(src_seq_size, input_size)
-        self.span_r_ptr_logits = Pointer(src_seq_size, input_size)
+    def forward(self, x):
+        return self.module(*x)
 
-        decoder_input_size = (self.op_embedding.embedding_dim + col_seq_size +
-                              col_seq_size + src_seq_size)
 
-        self.decoder_lstm = nn.LSTM(
-            decoder_input_size,
-            flags.FLAGS.decoder_size,
-            num_layers=1,
-            bidirectional=False)
+class _Sequential(nn.Sequential):
+    # nn.Sequential only allows one argument, this lets us
+    # create nn.Sequential instances which accept multiple
+    def forward(self, *args):
+        return super().forward(args)
 
-    @staticmethod
-    def dummy_input(stop=False):
-        """
-        stop, op, col, span_l, span_r
-        """
-        stop = 1 if stop else 0
-        return (stop, -1, -1, -1, -1)
 
-    @staticmethod
-    def _fetch_or_zero(seq, idx):
-        _, e = seq.size()
-        if idx < 0:
-            return torch.zeros((e, ), dtype=seq.dtype, device=get_device())
-        return seq[idx]
+class _Cat(nn.Module):
+    def forward(self, x):
+        return torch.cat(x)
 
-    @staticmethod
-    def create_initial_state(initial_state_e):
-        """Given a vector of size flags.FLAGS.decoder_size,
-        creates the corresponding initial RNN state"""
+
+class _InitDecoderState(nn.Module):
+    def forward(self, x):
         # just pytorch API massaging (need cells to be
         # initialized as well; not just hidden state)
-        initial_decoder_state_11e = initial_state_e.view(1, 1, -1)
+        # given a vector of the size of the decoder states, creates an emptyu
+        initial_decoder_state_11e = x.view(1, 1, -1)
         initial_decoder_state = (initial_decoder_state_11e,
                                  torch.zeros_like(initial_decoder_state_11e))
         return initial_decoder_state
 
-    def forward(self, hidden_state, decoder_input, sqe_qe, sce_ce):
+
+class ConditionDecoder(nn.Module):
+    """
+    The decoder produces seq2seq-style output for the conditional
+    columns in WikiSQL.
+
+    It process input in several stages.
+
+    The decoder accepts the question and table sequence encodings.
+
+    Then we perform the following in order:
+    1. the decoder predicts the number of predicates with a feed-forward
+       network attending to the input sequences.
+    2. the decoder predicts which columns to predicate on with an RNN
+       with state initialized from the joint summary (automatically
+       stopped after the number of predictions specified by (1)). Input
+       is augmented with attention over the input sequences.
+    3. Same as above, but we predict the comparison operators.
+    4. Same as above, but we predict the literals.
+
+    Each successive step uses the previous one as input.
+    """
+
+    def __init__(self, col_seq_size, src_seq_size, max_conds):
+        super().__init__()
+
+        # predict stop
+        joint_embedding_size = col_seq_size + src_seq_size
+        self.stop_logits = _Sequential(
+            _Unwrap(IndependentAttention(col_seq_size, src_seq_size, 0)),
+            _Cat(), MLP(joint_embedding_size, [], max_conds))
+
+        # predict cols
+        decoder_size = flags.FLAGS.decoder_size
+        self.col_init = _Sequential(
+            _Unwrap(IndependentAttention(col_seq_size, src_seq_size, 0)),
+            _Cat(), MLP(joint_embedding_size, [], decoder_size),
+            _InitDecoderState())
+        self.col_attn = _double_attn(col_seq_size, src_seq_size, decoder_size)
+        input_size = joint_embedding_size  # attention over inputs
+        input_size += col_seq_size  # feed own outputs
+        self.col_rnn = nn.LSTM(
+            input_size, decoder_size, num_layers=1, bidirectional=False)
+        self.col_proj = Pointer(col_seq_size, decoder_size)
+
+        # predict ops
+        self.op_init = _Sequential(
+            _Unwrap(IndependentAttention(col_seq_size, src_seq_size, 0)),
+            _Cat(), MLP(joint_embedding_size, [], decoder_size),
+            _InitDecoderState())
+        self.op_attn = _double_attn(col_seq_size, src_seq_size, decoder_size)
+        num_ops = len(wikisql.CONDITIONAL)
+        self.op_embedding = nn.Embedding(num_ops,
+                                         flags.FLAGS.op_embedding_size)
+        input_size = joint_embedding_size  # attention over inputs
+        input_size += col_seq_size  # feed predicted column
+        input_size += self.op_embedding.embedding_dim  # feed predicted op
+        self.op_rnn = nn.LSTM(
+            input_size, decoder_size, num_layers=1, bidirectional=False)
+        self.op_proj = MLP(decoder_size, [], num_ops)
+
+        # predict spans
+        self.span_init = _Sequential(
+            _Unwrap(IndependentAttention(col_seq_size, src_seq_size, 0)),
+            _Cat(), MLP(joint_embedding_size, [], decoder_size),
+            _InitDecoderState())
+        self.span_attn = _double_attn(col_seq_size, src_seq_size, decoder_size)
+        input_size = joint_embedding_size  # attention over inputs
+        input_size += col_seq_size  # feed predicted column
+        input_size += self.op_embedding.embedding_dim  # feed predicted op
+        # don't feed own inputs, not sure how to do this right yet
+        # input_size += 2 * src_seq_size
+        self.span_rnn = nn.LSTM(
+            input_size, decoder_size, num_layers=1, bidirectional=False)
+        self.span_l_ptr_logits = Pointer(src_seq_size, decoder_size)
+        self.span_r_ptr_logits = Pointer(src_seq_size, decoder_size)
+
+    def forward(self, sqe_qe, sce_ce, prepared_ex):
         """
         e = some embedding dimension, varies tensor to tensor
         i = decoder input dimension
         q = question length
+        q1 = question length + 1
         c = num columns
         o = cardinality of binary comparison operators
 
         sqe is the sequence of question word embeddings
         sce is the sequence of column description embeddings
 
-        returns the decoder output and next hidden state, where
-        the decoder output is a tuple containing
-
-        * whether the decoder should terminate (2 logits, 0 is don't
-          stop, 1 is to stop on the current input, i.e., don't produce
-          the values below).
-        * predicted logits for the next operator
-        * logits for the next column being conditioned on
-        * logits for the beginning of the span of the question
-          defining the literal
-        * logits for the end of the span
-
+        Returns a tuple with the following information (shape in parens)
+        * (max_conds) logits for the predicted number of condition columns, p
+        * (pc) logits over the column sequence for selected columns
+        * (po) logits over operators
+        * (pq) logits over left endpoint for span of literals
+        * (pq1) logits over right endpoint for span of literals
         """
-        stop, op, col, _span_l, _span_r = decoder_input
-        if stop:
-            ncols = len(sce_ce)
-            nwords = len(sqe_qe)
-            return self._stop_logits(ncols, nwords), hidden_state
+        # TODO separate sequence inputs for these three
+        # TODO op_encoding for op/span predictions
 
-        op_idx = self._optensor(op)
-        op_e = self.op_embedding(op_idx)
-        col_e = self._fetch_or_zero(sce_ce, col)
-        # TODO figure out a good way of feeding span_l and span_r as inputs
-        # to the decoder.
+        stop = self.stop_logits(sce_ce, sqe_qe)
 
-        hidden_cells_11e, _ = hidden_state
-        attn_context = hidden_cells_11e.view(-1)
-        attended_src, attended_col = self.src_col_attn(sqe_qe, sce_ce,
-                                                       attn_context)
-        decoder_input_11i = torch.cat(
-            [op_e, col_e, attended_src, attended_col]).view(1, 1, -1)
+        if self.training:
+            seq_len = len(prepared_ex['cond_op'])
+        else:
+            seq_len = stop.argmax().cpu().detach().numpy()
 
-        decoder_output_11e, hidden_state = self.decoder_lstm(
-            decoder_input_11i, hidden_state)
+        col_pc = []
+        h = self.col_init(sce_ce, sqe_qe)
+        prev_col = [torch.zeros_like(sce_ce[0])]
+        for i in range(seq_len):
+            rnn_input = torch.cat(
+                [self.col_attn(sce_ce, sqe_qe, h[0].view(-1)),
+                 prev_col[-1]]).view(1, 1, -1)
+            out, h = self.col_rnn(rnn_input, h)
+            col_c = self.col_proj(sce_ce, out.view(-1))
+            col_pc.append(col_c)
+            col = col_c.argmax().cpu().detach().numpy()
+            if self.training:
+                true_col = prepared_ex['cond_col'][i]
+                prev_col.append(sce_ce[true_col].detach())
+            else:
+                prev_col.append(sce_ce[col].detach())
+        prev_col = prev_col[1:]
 
-        context = decoder_output_11e.view(-1)
-        stop_2 = self.stop_logits(context)
+        op_po = []
+        h = self.op_init(sce_ce, sqe_qe)
+        prev_op = [
+            torch.zeros(self.op_embedding.embedding_dim, device=get_device())
+        ]
+        for i in range(seq_len):
+            rnn_input = torch.cat([
+                self.op_attn(sce_ce, sqe_qe, h[0].view(-1)), prev_col[i],
+                prev_op[-1]
+            ]).view(1, 1, -1)
+            out, h = self.op_rnn(rnn_input, h)
+            op_o = self.op_proj(out.view(-1))
+            op_po.append(op_o)
+            op = op_o.argmax().detach()
+            if self.training:
+                true_op = prepared_ex['cond_op'][i]
+                prev_op.append(self.op_embedding(true_op))
+            else:
+                prev_op.append(self.op_embedding(op))
+        prev_op = prev_op[1:]
 
-        op_logits_o = self.op_logits(context)
+        l_span_pq = []
+        r_span_pq1 = []
+        h = self.span_init(sce_ce, sqe_qe)
+        for _ in range(seq_len):
+            rnn_input = torch.cat([
+                self.span_attn(sce_ce, sqe_qe, h[0].view(-1)), prev_col[i],
+                prev_op[i]
+            ]).view(1, 1, -1)
+            out, h = self.span_rnn(rnn_input, h)
+            context = out.view(-1)
+            l_span_q = self.span_l_ptr_logits(sqe_qe, context)
+            l_span_pq.append(l_span_q)
+            with torch.no_grad():
+                # r = number entries after l
+                l = l_span_q.argmax().detach().cpu().numpy()
+                left_pad = [self._neg100()] * (l + 1)
+            sqe_re = sqe_qe[l:]
+            r_span_r = self.span_r_ptr_logits(sqe_re, context)
+            r_span_q1 = torch.cat(left_pad + [r_span_r])
+            r_span_pq1.append(r_span_q1)
 
-        context = torch.cat([context, op_logits_o.detach()])
-        col_logits_c = self.col_ptr_logits(sce_ce, context)
-
-        # Attend to column inputs as context for question pointing
-        col_attn_e = sce_ce.t().mv(F.softmax(col_logits_c, dim=0))
-        context = torch.cat([context, col_attn_e])
-        span_l_logits_q = self.span_l_ptr_logits(sqe_qe, context)
-
-        with torch.no_grad():
-            # r = number entries after l
-            l = span_l_logits_q.argmax().detach().cpu().numpy()
-            left_pad = [self._neg100()] * (l + 1)
-        sqe_re = sqe_qe[l:]
-        span_r_logits_r = self.span_r_ptr_logits(sqe_re, context)
-        span_r_logits_q1 = torch.cat(left_pad + [span_r_logits_r])
-
-        return (stop_2, op_logits_o, col_logits_c, span_l_logits_q,
-                span_r_logits_q1), hidden_state
-
-    @staticmethod
-    def _zeros(*shape):
-        return torch.zeros(shape, device=get_device())
-
-    @functools.lru_cache(maxsize=None)
-    def _stop_logits(self, ncols, nwords):
-        stop_2 = self._zeros(2)
-        stop_2[1] = 1
-        op_logits_o = self._zeros(len(wikisql.CONDITIONAL))
-        col_logits_c = self._zeros(ncols)
-        span_l_logits_q = self._zeros(nwords)
-        span_r_logits_q = self._zeros(nwords + 1)
-        return (stop_2, op_logits_o, col_logits_c, span_l_logits_q,
-                span_r_logits_q)
+        outs = (col_pc, op_po, l_span_pq, r_span_pq1)
+        outs = tuple(map(_stack, outs))
+        return (stop, ) + outs
 
     @staticmethod
     @functools.lru_cache(maxsize=1)
     def _neg100():
         return -100 * torch.ones([1], dtype=torch.float32, device=get_device())
 
-    @staticmethod
-    @functools.lru_cache(maxsize=None)
-    def _optensor(op):
-        x = torch.ones([], dtype=torch.long, device=get_device())
-        return x * max(op, 0)
+
+def _stack(x):
+    if x:
+        return torch.stack(x)
+    return torch.Tensor([])
--- asn4sql/wikisql_specific/wikisql_specific_model.py
+++ asn4sql/wikisql_specific/wikisql_specific_model.py
@@ -24,53 +24,19 @@ for the three separate wikisql prediction tasks:
 * selection column prediction
 * conjunctive condition prediction
 
-Currently, a question encoding transforms a sequence into an encoded sequence
-through the hidden states of a bidirectional LSTM. A summary of the
-sequence is also offered.
-
-In the case of the columns, however, a bidirectional LSTM is also applied
-to every column description (the same LSTM, applied separately to each column).
-The final hidden state for each column is used to construct the
-sequence column encoding.
-
-The final question encoding conditions attention over the sequence column
-encoding, producing a final column encoding.
-
-The final column encoding and final question encodings are fed into an MLP
-for aggregation classification.
-
-We apply a pointer network conditioned on the final question encoding
-over the sequence column encoding
-to obtain a selection classifier.
-
-The final column and question encodings are fed into a network which
-initializes the decoder state.
-
-Starting from initialization, the decoder produces hidden states
-that are then used to:
-
-(0) whether the decoder should terminate.
-(1) condition a column pointer network to select a column over
-    the SCE.
-(2) conditioned on the selected column, select a conditional op
-(3) condition a pointer network with (1) and (2) to select a
-    left and then right endpoint for a literal over the SQE.
-
-(0), (1), (2), and (3) are fed back as inputs into the decoder; this
-is teacher-forced during training.
-
+TODO rest of arch.
+TODO use more Sequential where possible.
 """
 
 import sys
 
 from absl import flags
-import numpy as np
 import torch
 from torch import nn
 from torch.nn import functional as F
 
 from .mlp import MLP
-from .double_attention import double_attention
+from .double_attention import IndependentAttention
 from .nl_embedding import NLEmbedding
 from .column_encoding import ColumnEncoding
 from .question_encoding import QuestionEncoding
@@ -116,13 +82,10 @@ class WikiSQLSpecificModel(nn.Module):
         self.column_encoding_for_cond = ColumnEncoding(
             natural_language_embedding, tbl_field)
 
-        self.agg_attention = double_attention(
+        self.agg_attention = IndependentAttention(
             self.column_encoding_for_agg.sequence_size,
             self.question_encoding_for_agg.sequence_size, 0)
-        self.sel_attention = double_attention(
-            self.column_encoding_for_cond.sequence_size,
-            self.question_encoding_for_cond.sequence_size, 0)
-        self.cond_attention = double_attention(
+        self.sel_attention = IndependentAttention(
             self.column_encoding_for_cond.sequence_size,
             self.question_encoding_for_cond.sequence_size, 0)
 
@@ -133,11 +96,9 @@ class WikiSQLSpecificModel(nn.Module):
                                    len(wikisql.AGGREGATION))
         self.selection_ptrnet = Pointer(
             self.column_encoding_for_sel.sequence_size, joint_embedding_size)
-        self.decoder_initialization_mlp = MLP(joint_embedding_size, [],
-                                              flags.FLAGS.decoder_size)
         self.condition_decoder = ConditionDecoder(
             self.column_encoding_for_cond.sequence_size,
-            self.question_encoding_for_cond.sequence_size)
+            self.question_encoding_for_cond.sequence_size, _MAX_COND_LENGTH)
 
     def prepare_example(self, ex):
         """
@@ -225,61 +186,11 @@ class WikiSQLSpecificModel(nn.Module):
                                             prepared_ex['ent']))
         sequence_column_encoding_for_cond_ce = self.column_encoding_for_sel(
             prepared_ex['tbl'])
-        joint_encoding_for_cond_e = torch.cat(
-            self.cond_attention(sequence_column_encoding_for_cond_ce,
-                                sequence_question_encoding_for_cond_qe))
-        initial_decoder_state = self.decoder_initialization_mlp(
-            joint_encoding_for_cond_e)
-        initial_decoder_state = self.condition_decoder.create_initial_state(
-            initial_decoder_state)
-        conds = self._predicted_conds(
-            initial_decoder_state, sequence_question_encoding_for_cond_qe,
-            sequence_column_encoding_for_cond_ce, prepared_ex)
+        conds = self.condition_decoder(sequence_question_encoding_for_cond_qe,
+                                       sequence_column_encoding_for_cond_ce,
+                                       prepared_ex)
 
         return aggregation_logits_a, selection_logits_c, conds
-
-    def _predicted_conds(self, hidden_state, question, columns, prepared_ex):
-        stop_w2 = []
-        op_logits_wo = []
-        col_logits_wc = []
-        span_l_logits_wq = []
-        span_r_logits_wq1 = []
-
-        true_cond_fields = [
-            'cond_op', 'cond_col', 'cond_span_l', 'cond_span_r'
-        ]
-        num_cols = len(prepared_ex['cond_op'])
-        decoder_input = self.condition_decoder.dummy_input(stop=False)
-        for i in range(_MAX_COND_LENGTH):
-            decoder_output, hidden_state = self.condition_decoder(
-                hidden_state, decoder_input, question, columns)
-            (pred_stop_2, pred_op_logits_o, pred_col_logits_c,
-             pred_span_l_logits_q, pred_span_r_logits_q1) = decoder_output
-            stop_w2.append(pred_stop_2)
-            op_logits_wo.append(pred_op_logits_o)
-            col_logits_wc.append(pred_col_logits_c)
-            span_l_logits_wq.append(pred_span_l_logits_q)
-            span_r_logits_wq1.append(pred_span_r_logits_q1)
-
-            # prepare the next input: see ConditionDecoder.dummy_input
-            # for the expected order of inputs
-            if self.training:
-                # teacher force
-                if i < num_cols:
-                    stop = 0
-                    decoder_input = (stop, ) + tuple(prepared_ex[x][i]
-                                                     for x in true_cond_fields)
-                else:
-                    decoder_input = self.condition_decoder.dummy_input(
-                        stop=True)
-            else:
-                with torch.no_grad():
-                    decoder_input = tuple(x.argmax() for x in decoder_output)
-        return tuple(
-            map(torch.stack, [
-                op_logits_wo, col_logits_wc, span_l_logits_wq,
-                span_r_logits_wq1, stop_w2
-            ]))
 
     def forward(self, prepared_ex):
         """
@@ -292,21 +203,20 @@ class WikiSQLSpecificModel(nn.Module):
         acc = results['acc (*total)']
         return loss, acc
 
-    @staticmethod
-    def _truncated_nll_acc(input_il, target_o):
-        # i = input length
-        # o = output length
-        assert len(input_il) >= len(target_o), (len(input_il), len(target_o))
+    def _truncated_nll_acc(self, input_il, target_o):
         # m = min(i, o)
         # l = logits size (so target is in the range [0, o))
         m = min(len(input_il), len(target_o))
+        if self.training:
+            # should be teacher forced
+            assert m == len(input_il), (m, len(input_il), target_o)
         if not m:
             return (torch.zeros([], device=get_device()),
                     torch.ones([], device=get_device()))
         input_ml = input_il[:m]
         target_m = target_o[:m]
         nll = F.cross_entropy(input_ml, target_m)
-        acc = torch.prod(input_ml.argmax(dim=1) == target_o).type(
+        acc = torch.prod(input_ml.argmax(dim=1) == target_m).type(
             torch.float32)
         return nll, acc
 
@@ -324,22 +234,20 @@ class WikiSQLSpecificModel(nn.Module):
         """
         (aggregation_logits_a, selection_logits_c,
          conds) = self.predict(prepared_ex)
-        (op_logits_wo, col_logits_wc, span_l_logits_wq, span_r_logits_wq1,
-         stop_w2) = conds
+        stop, col_pc, op_po, l_span_pq, r_span_pq1 = conds
         ncols = len(prepared_ex['cond_op'])
-        true_stop = torch.zeros(
-            [_MAX_COND_LENGTH], dtype=torch.long).to(get_device())
-        true_stop[ncols:] = 1
 
-        op_loss, op_acc = self._truncated_nll_acc(op_logits_wo,
+        op_loss, op_acc = self._truncated_nll_acc(op_po,
                                                   prepared_ex['cond_op'])
-        col_loss, col_acc = self._truncated_nll_acc(col_logits_wc,
+        col_loss, col_acc = self._truncated_nll_acc(col_pc,
                                                     prepared_ex['cond_col'])
         span_l_loss, span_l_acc = self._truncated_nll_acc(
-            span_l_logits_wq, prepared_ex['cond_span_l'])
+            l_span_pq, prepared_ex['cond_span_l'])
         span_r_loss, span_r_acc = self._truncated_nll_acc(
-            span_r_logits_wq1, prepared_ex['cond_span_r'])
-        stop_loss, stop_acc = self._truncated_nll_acc(stop_w2, true_stop)
+            r_span_pq1, prepared_ex['cond_span_r'])
+        stop_loss, stop_acc = self._truncated_nll_acc(
+            stop.unsqueeze(0),
+            torch.tensor([ncols], dtype=torch.long, device=get_device()))  # pylint: disable=not-callable
 
         cond_acc = torch.prod(
             torch.stack([op_acc, col_acc, span_l_acc, span_r_acc, stop_acc]))
@@ -354,18 +262,15 @@ class WikiSQLSpecificModel(nn.Module):
         sel_acc = sel_acc.type(torch.float32)
 
         if with_prediction:
-            stop = stop_w2.argmax(1).detach().cpu().numpy()
-            stop = stop.argmax() if np.any(stop) else None
-            ops = op_logits_wo.argmax(1).detach().cpu().numpy()[:stop]
-            cols = col_logits_wc.argmax(1).detach().cpu().numpy()[:stop]
-            span_l = span_l_logits_wq.argmax(1).detach().cpu().numpy()[:stop]
-            span_r = span_r_logits_wq1.argmax(1).detach().cpu().numpy()[:stop]
+            stop = stop.argmax().cpu().detach().numpy()
+            ops = _argmax1(op_po).detach().cpu().numpy()[:stop]
+            cols = _argmax1(col_pc).detach().cpu().numpy()[:stop]
+            span_l = _argmax1(l_span_pq).detach().cpu().numpy()[:stop]
+            span_r = _argmax1(r_span_pq1).detach().cpu().numpy()[:stop]
             agg = aggregation_logits_a.argmax().detach().cpu().numpy()
             sel = selection_logits_c.argmax().detach().cpu().numpy()
             prediction = wikisql.Prediction(ops, cols, span_l, span_r, agg,
                                             sel)
-            prediction.stop = list(stop_w2.argmax(1).detach().cpu().numpy())
-            prediction.stop_true = list(true_stop.detach().cpu().numpy())
         else:
             prediction = None
 
@@ -387,3 +292,9 @@ class WikiSQLSpecificModel(nn.Module):
                 if hasattr(out[k], 'detach'):
                     out[k] = out[k].detach().cpu().numpy()
         return out
+
+
+def _argmax1(x):
+    if x.numel():
+        return x.argmax(1)
+    return x
